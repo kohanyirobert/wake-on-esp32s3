ef2e8f361e2c057f83e6bbf194762ef466cf3de3
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-09 23:04:49 +0100

    Use mDNS and WiFi events

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void loop()
 #else
 
 #include <Arduino.h>
+#include <ESPmDNS.h>
 #include <FastLED.h>
 #include <USB.h>
 #include <USBHIDKeyboard.h>
@@ -19,33 +20,23 @@ void loop()
 #include "wifi_setup.h"
 
 #define NUM_LEDS 1
+#define BAUD_RATE 9600
+#define HTTP_PORT 80
+#define DOMAIN_NAME "wake-on-esp32s3"
 
 CRGB leds[NUM_LEDS];
-WebServer server(80);
 USBHIDKeyboard Keyboard;
+WebServer server(HTTP_PORT);
 
 void setup()
 {
     delay(5000);
-    Serial.begin(9600);
-    USB.begin();
-    FastLED.clear();
-    FastLED.addLeds<APA102, LED_DI_PIN, LED_CI_PIN, BGR>(leds, NUM_LEDS);
-    Serial.print("connecting to ");
-    Serial.print(WIFI_SSID);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.println();
-    Serial.print("connected at ");
-    Serial.println(WiFi.localIP());
-    Serial.end();
-    server.on("/", []() {
+    server.on("/", HTTP_POST, []() {
+        Serial.begin(BAUD_RATE);
+        Serial.println("got message");
+        Serial.end();
         server.send(200);
-        leds[0] = CRGB::Green;
+        leds[0] = CRGB::Blue;
         FastLED.show();
         Keyboard.begin();
         Keyboard.press(KEY_F24);
@@ -55,7 +46,64 @@ void setup()
         leds[0] = CRGB::Black;
         FastLED.show();
     });
-    server.begin();
+    USB.begin();
+    FastLED.clear();
+    FastLED.addLeds<APA102, LED_DI_PIN, LED_CI_PIN, BGR>(leds, NUM_LEDS);
+    Serial.begin(BAUD_RATE);
+    Serial.print("connecting to ");
+    Serial.println(WIFI_SSID);
+    Serial.end();
+    WiFi.disconnect(true);
+    WiFi.onEvent([](WiFiEvent_t event) {
+        switch (event)
+        {
+        case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+            Serial.begin(BAUD_RATE);
+            Serial.print("connected at ");
+            Serial.println(WiFi.localIP());
+            Serial.end();
+            server.begin();
+            if (MDNS.begin(DOMAIN_NAME))
+            {
+                Serial.begin(BAUD_RATE);
+                Serial.println("set up mDNS");
+                Serial.end();
+            }
+            else
+            {
+                Serial.begin(BAUD_RATE);
+                Serial.println("couldn't set up mDNS");
+                Serial.end();
+            }
+            MDNS.addService("http", "tcp", HTTP_PORT);
+            leds[0] = CRGB::Green;
+            FastLED.show();
+            Keyboard.begin();
+            Keyboard.press(KEY_F24);
+            Keyboard.release(KEY_F24);
+            Keyboard.end();
+            delay(500);
+            leds[0] = CRGB::Black;
+            FastLED.show();
+            break;
+        case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+            MDNS.end();
+            leds[0] = CRGB::Red;
+            FastLED.show();
+            delay(500);
+            leds[0] = CRGB::Black;
+            FastLED.show();
+            Serial.begin(BAUD_RATE);
+            Serial.print("disconnected, reconnecting to ");
+            Serial.println(WIFI_SSID);
+            Serial.end();
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+            break;
+        default:
+            break;
+        }
+    });
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop()
