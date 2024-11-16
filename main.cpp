1ddc43f5cf445a47dcd73b3451dc83f480d7736a
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-16 15:26:29 +0100

    Organize LED blinks and defined WAITs

--- main.cpp
+++ main.cpp
@@ -19,6 +19,10 @@ void loop()
 #include "pin_config.h"
 #include "wifi_setup.h"
 
+#define WAIT_HUGE 5000
+#define WAIT_NORMAL 1000
+#define WAIT_SMALL 500
+#define WAIT_TINY 100
 #define NUM_LEDS 1
 #define BAUD_RATE 9600
 #define HTTP_PORT 80
@@ -30,19 +34,19 @@ WebServer server(HTTP_PORT);
 
 void setup()
 {
-    delay(5000);
+    delay(WAIT_HUGE);
     server.on("/", HTTP_POST, []() {
+        leds[0] = CRGB::Blue;
+        FastLED.show();
         Serial.begin(BAUD_RATE);
         Serial.println("got message");
         Serial.end();
         server.send(200);
-        leds[0] = CRGB::Blue;
-        FastLED.show();
         Keyboard.begin();
         Keyboard.press(KEY_F24);
         Keyboard.release(KEY_F24);
         Keyboard.end();
-        delay(500);
+        delay(WAIT_SMALL);
         leds[0] = CRGB::Black;
         FastLED.show();
     });
@@ -54,10 +58,13 @@ void setup()
     Serial.println(WIFI_SSID);
     Serial.end();
     WiFi.disconnect(true);
+    delay(WAIT_NORMAL);
     WiFi.onEvent([](WiFiEvent_t event) {
         switch (event)
         {
         case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+            leds[0] = CRGB::Green;
+            FastLED.show();
             Serial.begin(BAUD_RATE);
             Serial.print("connected at ");
             Serial.println(WiFi.localIP());
@@ -76,24 +83,26 @@ void setup()
                 Serial.end();
             }
             MDNS.addService("http", "tcp", HTTP_PORT);
-            leds[0] = CRGB::Green;
-            FastLED.show();
-            delay(500);
+            delay(WAIT_SMALL);
             leds[0] = CRGB::Black;
             FastLED.show();
+            delay(WAIT_NORMAL);
             break;
         case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
             MDNS.end();
             leds[0] = CRGB::Red;
             FastLED.show();
-            delay(500);
-            leds[0] = CRGB::Black;
-            FastLED.show();
             Serial.begin(BAUD_RATE);
             Serial.print("disconnected, reconnecting to ");
             Serial.println(WIFI_SSID);
             Serial.end();
+            WiFi.disconnect(true);
+            delay(WAIT_NORMAL);
             WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+            delay(WAIT_SMALL);
+            leds[0] = CRGB::Black;
+            FastLED.show();
+            delay(WAIT_NORMAL);
             break;
         default:
             break;
@@ -105,6 +114,6 @@ void setup()
 void loop()
 {
     server.handleClient();
-    delay(100);
+    delay(WAIT_TINY);
 }
 #endif
